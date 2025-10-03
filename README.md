6d488a43d6c000799fdafd45176f93019611e165
elias <elias.gauthier@etu.u-bordeaux.fr>
2025-10-03 22:53:48 +0200

    Update qubit signatures, readme and rename docs to assets

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 <div align="center">
    <img src="https://github.com/elias-utf8/qcaml/blob/main/docs/images/qcaml_banner.png" alt="QCaml logo" width="350"/>
-   
+
    Experimental cross-platform ocaml library for quantum computing
-   
+
    [![License](https://img.shields.io/badge/License-Apache%202.0-3c60b1.svg?logo=opensourceinitiative&logoColor=white&style=flat-square)](https://github.com/elias-utf8/qcaml/blob/main/LICENSE)
    [![OCaml](https://img.shields.io/badge/OCaml-4.13+-ec6813.svg?style=flat-square&logo=ocaml&logoColor=white)](https://ocaml.org/)
-   
+
 </div>
 
 ## Presentation
--- lib/qubit.mli
+++ lib/qubit.mli
@@ -1,9 +1,9 @@
 (**
-  @file        qubit.mli
-  @author      Elias GAUTHIER
-  @date        2025-10-02
-  @license     Apache-2.0
-  @description Implementation of qubits with quantum state representation and operations.
+  @author       Elias GAUTHIER
+  file :        qubit.mli
+  date :        2025-10-02
+  license :     Apache-2.0
+  description : Implementation of qubits with quantum state representation and operations.
 *)
 
 (** Type complex with real and imaginary part *)
@@ -12,13 +12,36 @@ type complex = {
   im : float;
 }
 
-val czero : complex
-
-val cone : complex
-
 (** Complex multiplication
    @param c1 First complex number
    @param c2 Second complex number
    @return The product of c1 and c2
 *)
 val cmul : complex -> complex -> complex
+
+(** Complex addition
+   @param c1 First complex number
+   @param c2 Second complex number
+   @return The sum of c1 and c2
+*)
+val cadd : complex -> complex -> complex
+
+(** Complex subtraction
+   @param c1 First complex number
+   @param c2 Second complex number
+   @return The difference of c1 and c2
+*)
+val csub : complex -> complex -> complex
+
+(** Complex conjugate
+   @param c Complex number
+   @return The conjugate of c
+*)
+val cconj : complex -> complex
+
+(** Multiply complex by real scalar
+   @param r Real scalar
+   @param c Complex number
+   @return The product of r and c
+*)
+val cmul_scalar : float -> complex -> complex
