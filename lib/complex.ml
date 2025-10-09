b23ac01721ff1be42a2c76065947070d7134d6b4
elias <elias.gauthier@etu.u-bordeaux.fr>
2025-10-09 22:26:56 +0200

    Minor changes in complex.ml and complex.mli

--- lib/complex.ml
+++ lib/complex.ml
@@ -7,17 +7,18 @@ let zero = { re = 0.0; im = 0.0 }
 
 let one = { re = 1.0; im = 0.0 }
 
-let cmul c1 c2 =
-  { re = c1.re *. c2.re -. c1.im *. c2.im;
-    im = c1.re *. c2.im +. c1.im *. c2.re }
-
 let cadd c1 c2 =
   { re = c1.re +. c2.re;
     im = c1.im +. c2.im }
 
+let cmul c1 c2 =
+  { re = c1.re *. c2.re -. c1.im *. c2.im;
+    im = c1.re *. c2.im +. c1.im *. c2.re }
+
 let csub c1 c2 =
   { re = c1.re -. c2.re;
     im = c1.im -. c2.im }
+
 let cconj c =
   { re = c.re;
     im = -. c.im }
