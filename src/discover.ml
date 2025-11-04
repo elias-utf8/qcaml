95ffbfbf1632150deee9f15fd66a8e4deb012e36
elias <elias.gauthier@justice.fr>
2025-11-04 10:44:56 +0100

    Specify macosx for prevent BSD to hit Mac case

--- src/discover.ml
+++ src/discover.ml
@@ -8,16 +8,16 @@ let () =
         if String.contains os_type 'w' then
           (* Windows *)
           (["-I/mingw64/include"], ["-lfreeglut"; "-lglu32"; "-lopengl32"])
-        else if String.contains os_type 'd' || String.contains os_type 'm' then
+        else if os_type = "macosx" then
           (* macOS ARM and Intel *)
-          let include_path = 
+          let include_path =
             if architecture = "arm64" then "/opt/homebrew/include"
             else "/usr/local/include"
           in
           (["-I" ^ include_path], ["-framework"; "OpenGL"; "-framework"; "GLUT"])
         else
-          (* Linux and others *)
-          (["-I/usr/include"], ["-lglut"; "-lGLU"; "-lGL"])
+          (* Linux, FreeBSD and others *)
+          (["-I/usr/local/include"; "-I/usr/include"], ["-lglut"; "-lGLU"; "-lGL"])
       in
       C.Flags.write_sexp "c_flags.sexp" c_flags;
       C.Flags.write_sexp "c_library_flags.sexp" c_library_flags)
