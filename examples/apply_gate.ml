b4b4eec7ce1f58565f70340682bb9452e09c65a7
elias <elias.gauthier@etu.u-bordeaux.fr>
2026-01-10 13:44:42 +0100

    Graphics migration: remove bloch sphere visualisation

--- examples/apply_gate.ml
+++ examples/apply_gate.ml
@@ -3,5 +3,4 @@ open Quantum
 let () =
   let q = Qubit.zero () in
   Gate.h q;
-  Gate.t q;
-  Visualization.plot_bloch q
+  Gate.t q
--- examples/bloch_sphere.ml
+++ examples/bloch_sphere.ml
@@ -2,6 +2,5 @@ open Quantum
 
 let () =
   let q = Qubit.plus () in
-  Visualization.plot_bloch q;
   Measurement.measure q;
   Printf.printf "%s\n" (Qubit.print () q);
